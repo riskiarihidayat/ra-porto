beb521dbc14eb0be8662fab884ab0d97a06c4530
riskiarihidayat <riskiarihdyt@gmail.com>
2023-12-26 22:16:37 +0700

    feature/add about page in homepage.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,10 +12,10 @@ const Navbar = () => {
   // let [isOpen, setisOpen] = useState(false)
 
   return (
-      <nav className="fixed w-full z-20 top-0 start-0 py-2 sm:py-4 md:py-[18px] lg:py-[27px] bg-secondary-1">
+      <nav className="section-padding fixed w-full z-20 top-0 start-0 py-2 sm:py-4 md:py-[18px] lg:py-[27px] bg-secondary-1">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <Link to='/' className="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src={logo} className="h-8" alt="Flowbite Logo"/>
+            <LazyLoadImage src={logo} className="h-8" alt="Flowbite Logo"/>
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Riski Ari</span>
           </Link>
           <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse md:hidden">
--- src/homepage/About.jsx
+++ src/homepage/About.jsx
@@ -1,20 +1,23 @@
 import React from 'react'
 
-
+import myphotos from './../assets/ra.jpg'
+import { LazyLoadImage } from 'react-lazy-load-image-component'
 
 const About = () => {
   return (
-    <section className='text-center'>
-      <div>
-        <h3 className='mb-[26px] text-h3'>Let’s get know about me closer</h3>
-        <img src="" alt="Riski Ari" className='w-[300px] h-[370px] object-cover' />
-        <p className='mb-[76px] font-rubik font-normal md:text-lg text-[#A8A8A8] leading-[27px]'>I am an experienced software developer in front-end development and I have strong technical skills in JavaScript, HTML, and CSS.</p>
-        <button className='flex items-center gap-6 py-4 px-7 md:py-6 md:px-10 bg-primary rounded-full m-auto'>
-          <p className='font-rubik font-bold text-[13px] text-white'>Discover More About Me</p>
+    <section className='md:flex md:items-center md:gap-[78px] mb-[150px] md:mb-[290px]'>
+      <div className='text-center md:text-start'>
+        <h3 className='mb-[26px] text-h3 md:leading-[67px]'>Let’s get know about me closer</h3>
+        <LazyLoadImage src={myphotos} alt="Riski Ari" className='w-[300px] h-[250px] object-cover mb-[26px] md:hidden rounded-2xl' effect='blur' />
+        <p className='mb-[26px] md:mb-[76px] font-rubik font-normal md:text-lg text-[#A8A8A8] leading-[27px]'>I am an experienced software developer in front-end development and I have strong technical skills in JavaScript, HTML, and CSS.</p>
+        <button className='flex items-center gap-6 py-4 px-7 md:py-6 md:px-10 bg-primary rounded-full m-auto md:m-0'>
+          <p className='font-rubik font-bold text-[13px] md:text-base text-white'>Discover More About Me</p>
         </button>
       </div>
-      <div>
-        <img src="" alt="Riski Ari" className='hidden md:block md:w-[454px] md:h-[506px] md:object-cover' />
+      <div className='hidden relative md:block'>
+        <div className='absolute bottom-4 -left-10 w-[55px] h-[177px] rounded-full border-[1px] border-primary z-10'></div>
+        <div className='absolute -top-6 right-0 w-[110px] h-[34px] rounded-full border-[1px] border-primary z-10'></div>
+        <LazyLoadImage src={myphotos} alt="Riski Ari" className='md:w-[454px] md:object-cover rounded-2xl' effect='blur' />
       </div>
     </section>
   )
