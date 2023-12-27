955c0e2695490ca55a9f04a4545d1f0a9b153822
riskiarihidayat <riskiarihdyt@gmail.com>
2023-12-27 22:09:51 +0700

    feature/add testimonial section in homepage.jsx

--- src/homepage/HomePage.jsx
+++ src/homepage/HomePage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from './Hero'
 import About from './About'
 import Projects from './Projects'
+import Testimonial from './Testimonial'
 
 const HomePage = () => {
   return (
@@ -9,6 +10,7 @@ const HomePage = () => {
       <Hero />
       <About />
       <Projects />
+      <Testimonial />
     </div>
   )
 }
