9458388715033b1f574e27c21762147f6358ecd0
riskiarihidayat <riskiarihdyt@gmail.com>
2023-12-27 21:34:46 +0700

    feature/add project section in homepage.jsx

--- src/components/ProjectsCard.jsx
+++ src/components/ProjectsCard.jsx
@@ -1,25 +1,26 @@
 import React from 'react'
+import { LazyLoadImage } from 'react-lazy-load-image-component'
 
-const ProjectsCard = ({ img, title, client, clientDes, work, workDes1, workDes2 }) => {
+const ProjectsCard = ({ img, title, client, work1, work2 }) => {
   return (
     <div>
-      <div>
-        <img src={img} alt="Web Capture" />
+      <div className='mb-5 md:mb-8'>
+        <LazyLoadImage src={img} alt="Web Capture" className='w-[350px] md:w-[480px] object-cover' effect='blur' />
       </div>
       <div>
-        <div className='flex items-center gap-8'>
+        <div className='flex items-center gap-8 mb-2 md:mb-8'>
           <h4 className='font-syne font-bold text-2xl text-white leading-[32px] mb-8'>{title}</h4>
           <span className='w-[48px] h-[1px] bg-primary'></span>
         </div>
         <div>
           <div className='flex items-center gap-4'>
-            <p className='text-item-title'>{client}:</p>
-            <p className='text-item-des'>{clientDes}</p>
+            <p className='text-item-title'>Client:</p>
+            <p className='text-item-des'>{client}</p>
           </div>
           <div className='flex items-center gap-4'>
-            <p className='text-item-title'>{work}:</p>
-            <p className='text-item-des'>{workDes1}</p>
-            <p className='text-item-des'>{workDes2}</p>
+            <p className='text-item-title'>Work:</p>
+            <p className='text-item-des'>{work1}</p>
+            <p className='text-item-des'>{work2}</p>
           </div>
         </div>
       </div>
--- src/homepage/Projects.jsx
+++ src/homepage/Projects.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import { HiArrowNarrowRight } from "react-icons/hi"
 import ProjectsCard from '../components/ProjectsCard'
 
+import alnasr from './../assets/alnasr.jpg'
+import chat from './../assets/chat-n-rechat.jpg'
+import coffetto from './../assets/coffetto.jpg'
+
 const Projects = () => {
   return (
-    <section className='section-padding'>
+    <section className='section-padding mb-[166px]'>
       <div className='text-center mb-[80px]'>
         <h2 className='text-h2 mb-8 md:mb-[54px]'>My Projects Highlight</h2>
         <button className='flex items-center gap-3 md:gap-[29px] py-4 md:py-[26px] px-7 md:px-[45px] rounded-full border-[1px] border-primary m-auto'>
@@ -12,16 +16,39 @@ const Projects = () => {
           <HiArrowNarrowRight />
         </button>
       </div>
-      <div>
-        {/* <ProjectsCard
-          img={}
-          title=''
-          client=''
-          clientDes=''
-          work=''
-          workDes1=''
-          workDes2=''
-        /> */}
+      <div className='flex flex-col md:justify-center md:items-center'>
+        <div className='flex flex-col md:flex-row md:justify-between md:items-start gap-[27px] mb-[27px]'>
+          <ProjectsCard
+            img={alnasr}
+            title='Al Nasr Landing Page'
+            client='Alnasr'
+            work1='Landing Page'
+            work2='Responsive'
+          />
+          <ProjectsCard
+            img={chat}
+            title='Chat n Rechat Landing Page'
+            client='Nauv'
+            work1='Landing Page'
+            work2='Responsive'
+          />
+        </div>
+        <div className='flex flex-col md:flex-row md:justify-between md:items-start gap-[27px]'>
+          <ProjectsCard
+            img={coffetto}
+            title='Coffetto Landing Page'
+            client='Cof'
+            work1='Landing Page'
+            work2='Responsive'
+          />
+          <ProjectsCard
+            img={coffetto}
+            title='Coffetto Landing Page'
+            client='Cof'
+            work1='Landing Page'
+            work2='Responsive'
+          />
+        </div>
       </div>
     </section>
   )
